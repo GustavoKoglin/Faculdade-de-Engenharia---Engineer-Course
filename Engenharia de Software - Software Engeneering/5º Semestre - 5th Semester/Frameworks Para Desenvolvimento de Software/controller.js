3d649308db70c23dde819b1270fc969f6bfd8d5f
Gustavo Koglin <contato@devgustavokoglin.com.br>
2025-08-09 17:00:28 -0400

    Adicionando código da Aula Prrática sobre Frameworks

---
+++ Engenharia de Software - Software Engeneering/5º Semestre - 5th Semester/Frameworks Para Desenvolvimento de Software/controller.js
@@ -0,0 +1,24 @@
+function buscarCEP() {
+    const cep = document.getElementById('cep').value.replace(/\D/g, '');
+    if (cep.length !== 8) {
+        alert("CEP inválido!");
+        return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                alert("CEP não encontrado!");
+                return;
+            }
+            document.getElementById('rua').value = data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('cidade').value = data.localidade;
+            document.getElementById('estado').value = data.uf;
+        })
+        .catch(error => {
+            console.error("Erro na requisição:", error);
+            alert("Erro ao buscar CEP!");
+        });
+}
