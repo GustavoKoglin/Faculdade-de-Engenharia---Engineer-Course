3af5060b4a925e6d9776969fef1a9e31f66a33f1
Gustavo Koglin <contato@devgustavokoglin.com.br>
2025-08-09 16:09:50 -0400

    Atualizando arquivos

--- Engenharia de Computação - Computer Engineering/2º Semestre - 2nd Semester/Lista de Exercicios 2/Atividade 3/atividade3.py
+++
@@ -1,8 +0,0 @@
-try:
-    numero = int(input("Digite um número inteiro: "))
-    resultado = 10 / numero
-    print("Resultado da divisão: ", resultado)
-except ZeroDivisionError:
-    print("Erro: Não é possível dividir por zero.")
-except ValueError:
-    print("Erro: Digite um número inteiro válido.")
--- Engenharia de Computação - Computer Engineering/3º Semestre - 3rd Semester/Python Activities/README.md
+++
@@ -1,3 +0,0 @@
-List of exercises - Algorithms and Programming - 2nd Semester - Faculdade de Tecnologia de Sinop - FASTECH (PT-BR).
-
-Date: 06/18/2022
